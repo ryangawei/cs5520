7f8ad7b001ed5065a29d022244a9231720d88822
Guoao Wei <guoao.wei@outlook.com>
2023-02-14 14:46:37 -0800

    Create Home screen

--- App.js
+++ App.js
@@ -1,108 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, Button, Image, SafeAreaView, ScrollView, FlatList } from 'react-native';
 import { useState } from 'react';
-import Header from './components/Header';
-import Input from './components/Input';
-import GoalItem from './components/GoalItem';
+import Home from './Home';
 
 export default function App() {
-  const [enteredText, setEnteredText] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
-  const [goals, setGoals] = useState([]);
-
-  // This function is called on Confirm
-  function onTextEnter(text) {
-    console.log(text);
-    // setEnteredText(text);
-    const g = { text: text, id: Math.random() };
-    setGoals((prevGoals) => setGoals([...prevGoals, g]));
-    setModalVisible(false);
-  }
-
-  function onCancel() {
-    setModalVisible(false);
-  }
-
-  function deleteItem(id) {
-    setGoals((prevGoals) => setGoals(prevGoals.filter(item => item.id != id)));
-  }
-
-  function goalItemPressed(e, itemId) {
-    console.log(`Goal item ${itemId} pressed!`);
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-
-      <View style={styles.topContainer}>
-        <Header appName="Welcome to CS 5520 class" style={styles.header} />
-        <Button title="Add task" onPress={() => { setModalVisible(true); }}></Button>
-      </View>
-
-      <Input textUpdateFunction={onTextEnter} modalVisible={modalVisible} onCancel={onCancel} />
-
-      <View style={styles.bottomContainer}>
-        <FlatList
-          contentContainerStyle={styles.scrollViewContentContainer}
-          data={goals}
-          renderItem={({ item }) => { 
-            console.log(item); 
-            return (
-              <GoalItem item={item} deleteItem={deleteItem} goalItemPressed={goalItemPressed}/>
-            ) 
-            }}>
-        </FlatList>
-      {/* <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
-        { 
-          goals.map((g) => { 
-          return (
-            <View key={g.id} style={styles.textContainer}>
-              <Text style={styles.text}>{g.text}</Text> 
-            </View>
-          )
-        }) 
-        }
-      </ScrollView> */}
-      </View>
-    </SafeAreaView>
-  );
+    <Home />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  header: {
-    borderColor: "mediumorchid",
-    borderWidth: 1,
-    padding: 10,
-    color: "mediumorchid",
-    fontSize: 20
-  },
-  topContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  bottomContainer: {
-    flex: 4,
-    backgroundColor: "#f51251",
-  },
-  scrollViewContentContainer: {
-    alignItems: "center"
-  },
-  textContainer: {
-    borderRadius: 5,
-    backgroundColor: "#888",
-    marginVertical: 5,
-    padding: 5
-  },
-  text: {
-    color: "#4510ff",
-    fontSize: 30
-  }
-});
+
