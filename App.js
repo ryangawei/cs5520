cef0dec7fc131f7f76a6606e83769bd8a3bef534
Guoao Wei <guoao.wei@outlook.com>
2023-02-20 14:37:27 -0800

    Add screen header styles

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
-import { StyleSheet, Text, TextInput, View, Button, Image, SafeAreaView, ScrollView, FlatList } from 'react-native';
-import { useState } from 'react';
-import Home from './components/Home';
-import GoalDetails from './components/GoalDetails';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
+import Home from "./components/Home";
+import GoalDetails from "./components/GoalDetails";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home}></Stack.Screen>
-        <Stack.Screen name='GoalDetails' component={GoalDetails}></Stack.Screen>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "green" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontSize: 30 },
+        }}
+      >
+        <Stack.Screen
+          options={{ title: "All my goals" }}
+          name="Home"
+          component={Home}
+        ></Stack.Screen>
+        {/* Configuring Screen options dynamically */}
+        <Stack.Screen
+          name="GoalDetails"
+          component={GoalDetails}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
-
--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,15 +1,42 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Button } from "react-native";
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import PressableButton from "./PressableButton";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function GoalDetails({ route }) {
-  const navigation = useNavigation();
+
+export default function GoalDetails({ route, navigation }) {
+  useEffect(() => {
+    navigation.setOptions({
+      title: route.params.goal.text,
+      headerRight: () => {
+        return (
+          <PressableButton
+            onPress={() => {
+              console.log("Press top-right help");
+            }}
+          >
+            <AntDesign name="questioncircleo" size={24} color="black" />
+          </PressableButton>
+        );
+      },
+    });
+  }, []);
 
   console.log(route);
   return (
     <View>
-      <Text>{route.params.goal.id} - {route.params.goal.text}</Text>
-      <Button title="More details" onPress={() => navigation.navigate("GoalDetails", {"goal": route.params.goal})} />
+      {route.params && (
+        <Text>
+          {route.params.goal.id} - {route.params.goal.text}
+        </Text>
+      )}
+      <Button
+        title="More details"
+        onPress={() =>
+          navigation.navigate("GoalDetails", { goal: route.params.goal })
+        }
+      />
     </View>
-  )
+  );
 }
