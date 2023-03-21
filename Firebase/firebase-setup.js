db5d5de5f5c64104e2a1582cae927ee80072cca8
Guoao Wei <guoao.wei@outlook.com>
2023-03-21 14:53:11 -0700

    Update auth

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -3,6 +3,9 @@ import React from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId} from '@env';
+// import { getAuth } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { initializeAuth , getReactNativePersistence} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +25,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+// export const auth = getAuth(app);
+export const auth = initializeAuth(app, { persistence: getReactNativePersistence(AsyncStorage)});
