1a332f99668c9a938e549667c66faf02b967be5c
Guoao Wei <guoao.wei@outlook.com>
2023-04-04 16:26:39 -0700

    Simulate local notifications

--- components/Profile.js
+++ components/Profile.js
@@ -2,12 +2,15 @@ import { View, Text } from "react-native";
 import React from "react";
 import { auth } from "../Firebase/firebase-setup";
 import LocationManager from "./LocationManager";
+import NotificationManager from "./NotificationManager";
+
 export default function Profile() {
   return (
     <View>
       <Text>{auth.currentUser.email}</Text>
       <Text>{auth.currentUser.uid}</Text>
       <LocationManager />
+      <NotificationManager />
     </View>
   );
 }
